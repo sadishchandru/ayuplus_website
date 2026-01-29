8ea3c43e5e9d292d44f9066e7da63b4dc58cb98c
gnanaveljv <gnanaveljv@gmail.com>
2026-01-29 13:40:51 +0530

    Search engine icon created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,9 +66,14 @@ export const metadata: Metadata = {
     images: ["/images/leaf.svg"],
   },
   icons: {
-    icon: "/icon.svg",
-    shortcut: "/icon.svg",
-    apple: "/icon.svg",
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon.svg", type: "image/svg+xml" },
+      { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: "/apple-icon.png",
   },
 };
 
