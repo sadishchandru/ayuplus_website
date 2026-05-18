fa95774f3a3caa81a3a7de69c175837ecb020406
sadishchandru <sadish@gleamingsoftware.com>
2026-05-18 15:38:26 +0530

    fix: suppress hydration warning from browser extensions, fix image aspect ratio warnings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,6 +87,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${inter.variable} ${hedvigSerif.variable} antialiased`}
+        suppressHydrationWarning
       >
         {children}
         <ChatWidget />
